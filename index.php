48941b8d7205172024afc35f2b2e3a54748cb27d
lukelakea <lukelakea@gmail.com>
2017-11-17 19:49:33 -0500

    page now avoids broken image links

--- index.php
+++ index.php
@@ -1,75 +1,79 @@
 <?php
-	include "top.php";
+include "top.php";
 ?>
 
- <body>
+<body>
 
-<form onsubmit="return pingReddit();" class="my-form" action="index.php">
-    <select id="timeInput">
-      <option value="all">All Time</option>
-      <option value="year">Year</option>
-      <option value="month">Month</option>
-      <option value="week">Week</option>
-      <option value="day">Day</option>
-    </select>
 
-    <select id="subInputList">
-      <option value="accidentalrenaissance">Accidental Renaissance</option>
-      <option value="accidentalrockwell">Accidental Norman Rockwell</option>
-      <option value="accidentalwesanderson">Accidental Wes Anderson</option>
-      <option value="aerialporn">Aerial Views</option>
-      <option value="aestheticrain">Aesthetic Rain</option>
-      <option value="architectureporn">Architecture</option>
-      <option value="artporn">Alterantive Art</option>
-      <option value="art">Art</option>
-      <option value="autumnporn">Autumn</option>
-      <option value="bonsaiporn">Bonsai Trees</option>
-      <option value="botanicalporn">Botanical</option>
-      <option value="cats">Cats</option>
-      <option value="cemeteryporn">Cemeteries</option>
-      <option value="churchporn">Churches</option>
-      <option value="cinemagraphs">Cinemagraphs</option>
-      <option value="cityporn">Cities</option>
-      <option value="digitalpainting">Digital Painting</option>
-      <option value="design">Design</option>
-      <option value="dessertporn">Desserts</option>
-      <option value="earthporn">Earth</option>
-      <option value="fashionporn">Fashion</option>
-      <option value="foodporn">Food</option>
-      <option value="geologyporn">Geology</option>
-      <option value="gifs">Gifs</option>
-      <option value="historyporn">History</option>
-      <option value="houseporn">Houses</option>
-      <option value="illustration">Illustration</option>
-      <option value="imaginarybestof">Imaginary Best Of</option>
-      <option value="imaginarycharacters">Imaginary Characters</option>
-      <option value="imaginarylandscapes">Imaginary Landscapes</option>
-      <option value="imaginarymindscapes">Imaginary Mindscapes</option>
-      <option value="imaginarymonsters">Imaginary Monsters</option>
-      <option value="exposureporn">Long Exposure</option>
-      <option value="movieposterporn">Movie Posters</option>
-      <option value="painting">Painting</option>
-      <option value="pixelart">Pixel Art</option>
-      <option value="reactiongifs">Reaction Gifs</option>
-      <option value="spaceflightporn">Space Flight</option>
-      <option value="streetart">Street Art</option>      
-      <option value="technologyporn">Technology</option>
-      <option value="televisionposterporn">Television Posters</option>
-      <option value="trippy">Trippy</option>
-      <option value="unstirredpaint">Unstirred Paint</option>
-      <option value="villageporn">Villages</option>
-      <option value="waterporn">Water</option>
-      <option value="watercolor">Watercolor</option>
-      <option value="wildlifephotography">Wildlife Photography</option>
-      <option value="winterporn">Winter</option>
-      <option value="woahdude">Woah Dude</option>
-    </select>
-    <input type="text" name="name" id="subInputText" placeholder="Or Type a Subreddit Here!"/>
-    <input type="submit" value="Submit"">
+    <form onsubmit="return pingReddit();" class="my-form" action="index.php">
+        <div class="select">
+            <select id="timeInput">
+              <option value="all">All Time</option>
+              <option value="year">Year</option>
+              <option value="month">Month</option>
+              <option value="week">Week</option>
+              <option value="day">Day</option>
+          </select>
+      </div>
+      <div class="select">
+        <select id="subInputList">
+          <option value="accidentalrenaissance">Accidental Renaissance</option>
+          <option value="accidentalrockwell">Accidental Norman Rockwell</option>
+          <option value="accidentalwesanderson">Accidental Wes Anderson</option>
+          <option value="aerialporn">Aerial Views</option>
+          <option value="aestheticrain">Aesthetic Rain</option>
+          <option value="architectureporn">Architecture</option>
+          <option value="artporn">Alterantive Art</option>
+          <option value="art">Art</option>
+          <option value="autumnporn">Autumn</option>
+          <option value="bonsaiporn">Bonsai Trees</option>
+          <option value="botanicalporn">Botanical</option>
+          <option value="cats">Cats</option>
+          <option value="cemeteryporn">Cemeteries</option>
+          <option value="churchporn">Churches</option>
+          <option value="cinemagraphs">Cinemagraphs</option>
+          <option value="cityporn">Cities</option>
+          <option value="colorizedhistory">Colorized History</option>
+          <option value="digitalpainting">Digital Painting</option>
+          <option value="design">Design</option>
+          <option value="dessertporn">Desserts</option>
+          <option value="earthporn">Earth</option>
+          <option value="fashionporn">Fashion</option>
+          <option value="foodporn">Food</option>
+          <option value="geologyporn">Geology</option>
+          <option value="historyporn">History</option>
+          <option value="houseporn">Houses</option>
+          <option value="illustration">Illustration</option>
+          <option value="imaginarybestof">Imaginary Best Of</option>
+          <option value="imaginarycharacters">Imaginary Characters</option>
+          <option value="imaginarylandscapes">Imaginary Landscapes</option>
+          <option value="imaginarymindscapes">Imaginary Mindscapes</option>
+          <option value="imaginarymonsters">Imaginary Monsters</option>
+          <option value="exposureporn">Long Exposure</option>
+          <option value="movieposterporn">Movie Posters</option>
+          <option value="painting">Painting</option>
+          <option value="pixelart">Pixel Art</option>
+          <option value="reactiongifs">Reaction Gifs</option>
+          <option value="spaceflightporn">Space Flight</option>
+          <option value="streetart">Street Art</option>      
+          <option value="technologyporn">Technology</option>
+          <option value="televisionposterporn">Television Posters</option>
+          <option value="trippy">Trippy</option>
+          <option value="unstirredpaint">Unstirred Paint</option>
+          <option value="villageporn">Villages</option>
+          <option value="waterporn">Water</option>
+          <option value="watercolor">Watercolor</option>
+          <option value="wildlifephotography">Wildlife Photography</option>
+          <option value="winterporn">Winter</option>
+          <option value="woahdude">Woah Dude</option>
+      </select>
+  </div>
+  <input type="text" name="name" class="select" id="subInputText" placeholder="Or Type a Subreddit Here!"/>
+  <input type="submit" class="select submit" value="Submit"">
 </form>
 <div class="showimagediv"><img class="fullScreenimg"></></div>
 <div id="photoGrid">
-    </div>
+</div>
 <script type="text/javascript" >
     function pingReddit() {
         console.log(document.getElementById("timeInput").value);
@@ -83,8 +87,8 @@
 </script>
 
 
-    
-    <script type="text/javascript" src="bundle.js"></script>
+
+<script type="text/javascript" src="bundle.js"></script>
 </body>
 
 
@@ -93,6 +97,6 @@
 
 
 <?php
-	include "footer.php";
+include "footer.php";
 ?>
 </html>
--- snoowrap/app.js
+++ snoowrap/app.js
@@ -45,67 +45,72 @@ function doThingsWithImgLinks(imgLinks){
         
         var imgLinksArray = Array.from(imgLinks);
         document.getElementById('photoGrid').innerHTML ="";
-        for(i=0;i<imgLinks.length;i++){
-            var workingImgLink = imgLinksArray[i]
-            if(workingImgLink.search("ww.img")>=0){
-                workingImgLink = workingImgLink.replace("www.img","i.img");
-                workingImgLink += '.jpg'
-            }
-            if(workingImgLink.search("/imgur")>=0){
-                workingImgLink = workingImgLink.replace("/imgur","/i.imgur");
-                workingImgLink += '.jpg'
-            }
-            if(workingImgLink.search("gifv")>=0){
-                workingImgLink = workingImgLink.replace("gifv","gif");
-            }
-            if(workingImgLink.search("/gfy")>=0){
-                workingImgLink = workingImgLink.replace("/gfy","/zippy.gfy");
-                workingImgLink += '.webm'
-            }
-            
-             var img = new Image;
-            img.src = workingImgLink;
-            var bgImgWidth = img.width;
-            var bgImgHeight = img.height;
-             if(workingImgLink.search("webm")>=0){
-            document.getElementById('photoGrid').innerHTML += '<video autoplay loop class="box-wrapper"> <source  type="video/webm" src= "' + workingImgLink + '" data-url= "' + workingImgLink + '" class="box" style=background-image:url("' + workingImgLink + '")></video>';
-            }
-            // if(workingImgLink.search("webm")>=0){
-            //     <blockquote class="imgur-embed-pub" lang="en" data-id="a/Hlxpp"><a href="//imgur.com/Hlxpp"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
-            // }
-            else{
-            document.getElementById('photoGrid').innerHTML += '<div class="box-wrapper"> <div  data-url= "' + workingImgLink + '" class="box" style=background-image:url("' + workingImgLink + '")></div></div>';
+        for (i = 0; i < imgLinks.length; i++) {
+                  var workingImgLink = imgLinksArray[i]
 
-            }
-        }
-        $('.box-wrapper')
-        .on('mouseover', function(){
-          $(this).children('.box').css({'transform': 'scale(1.05)','z-index':'2'});
-        })
-        .on('mouseout', function(){
-          $(this).children('.box').css({'transform': 'scale(1)','z-index':'1'});
-        })
-        var imgToggle=false;
-        $('.box').on('click', function() {
-            var img = $('<img />', {src    : this.getAttribute("data-url"), 'class': 'fullScreenimg'});
+                  var img = new Image();
+                  
+                  img.src = workingImgLink;
+                  var bgImgWidth = img.width;
+                  var bgImgHeight = img.height;
+                  img.onload = function() {
 
-            if(imgToggle==true){
-                $('.showimagediv').html(img).hide();
-                imgToggle=false;
-            }
-            if(imgToggle==false){
-                $('.showimagediv').html(img).show();
-                imgToggle=true;
-            }
+                      if(workingImgLink.search("ww.img")>=0){
+                          workingImgLink = workingImgLink.replace("www.img","i.img");
+                          workingImgLink += '.jpg'
+                      }
+                      if(workingImgLink.search("/imgur")>=0){
+                          workingImgLink = workingImgLink.replace("/imgur","/i.imgur");
+                          workingImgLink += '.jpg'
+                      }
+                      if(workingImgLink.search("gifv")>=0){
+                          workingImgLink = workingImgLink.replace("gifv","gif");
+                      }
+                      if(workingImgLink.search("/gfy")>=0){
+                          workingImgLink = workingImgLink.replace("/gfy","/zippy.gfy");
+                          workingImgLink += '.webm'
+                      }
 
-        })
-        $('.showimagediv').on('click', function() {
-            var img = $('<img />', {src    : this.getAttribute("data-url"), 'class': 'fullScreenimg'});
-            if(imgToggle==true){
-                $('.showimagediv').html(img).hide();
-                imgToggle=false;
-            }
-        })
+                      if (this.width > 0) {
+                        if (this.src.search("webm") >= 0) {
+                          document.getElementById('photoGrid').innerHTML += '<video autoplay loop class="box-wrapper"> <source  type="video/webm" src= "' + this.src + '" data-url= "' + this.src + '" class="box" style=background-image:url("' + this.src + '")></video>';
+                        } else {
+                          document.getElementById('photoGrid').innerHTML += '<div class="box-wrapper"> <div  data-url= "' + this.src + '" class="box" style=background-image:url("' + this.src + '")></div></div>';
+
+                        }
+                      }
+
+                      $('.box-wrapper')
+                      .on('mouseover', function(){
+                        $(this).children('.box').css({'transform': 'scale(1.05)','z-index':'2'});
+                      })
+                      .on('mouseout', function(){
+                        $(this).children('.box').css({'transform': 'scale(1)','z-index':'1'});
+                      })
+                      var imgToggle=false;
+                      $('.box').on('click', function() {
+                          var img = $('<img />', {src    : this.getAttribute("data-url"), 'class': 'fullScreenimg'});
+
+                          if(imgToggle==true){
+                              $('.showimagediv').html(img).hide();
+                              imgToggle=false;
+                          }
+                          if(imgToggle==false){
+                              $('.showimagediv').html(img).show();
+                              imgToggle=true;
+                          }
+
+                      })
+                      $('.showimagediv').on('click', function() {
+                          var img = $('<img />', {src    : this.getAttribute("data-url"), 'class': 'fullScreenimg'});
+                          if(imgToggle==true){
+                              $('.showimagediv').html(img).hide();
+                              imgToggle=false;
+                          }
+                      })
+                  }
+                }
+        
 
     }
     
@@ -120,50 +125,44 @@ function doThingsWithImgLinks(imgLinks){
 
 
 
-/*
-var subInput = document.getElementById("subInput").getAttribute("data-name");
-var timeInput = document.getElementById("timeInput").getAttribute("data-name");
+/*        for (i = 0; i < imgLinks.length; i++) {
+          var workingImgLink = imgLinksArray[i]
 
-function doThingsWithImgLinks(imgLinks){
-        console.log(imgLinks);
-        
-        var imgLinksArray = Array.from(imgLinks);
-        document.getElementById('photoGrid').innerHTML ="";
-        for(i=0;i<imgLinks.length;i++){
-            var workingImgLink = imgLinksArray[i]
-            if(workingImgLink.search("/imgur")>=0){
-                // console.log('true');
-                workingImgLink.replace("/imgur","/i.imgur");
-                workingImgLink += '.jpg'
-            }
-            // console.log(workingImgLink);
-            var scaleDimension = (workingImgLink.width/workingImgLink.height);
-            console.log(scaleDimension);
-            var img = new Image;
-            img.src = workingImgLink;
-            var bgImgWidth = img.width;
-            var bgImgHeight = img.height;
-            if(bgImgHeight>bgImgWidth){
-                var imgRatio = bgImgHeight/bgImgWidth;
-                var widthLonger = false;
-            }
-            else{
-                var imgRatio = bgImgWidth/bgImgHeight;
-                var widthLonger = true;
-            }
-            console.log(bgImgHeight);
-            document.getElementById('photoGrid').innerHTML += '<div class="tile" data-height="' + bgImgHeight  +'" data-width="' + bgImgWidth  +'" data-width-longer="' + widthLonger + '" data-scale="' + imgRatio + '" data-image="' + workingImgLink + '"></div>';
+          var img = new Image();
+          
+          img.src = workingImgLink;
+          var bgImgWidth = img.width;
+          var bgImgHeight = img.height;
+          img.onload = async function() {
+            console.log(this);
+            console.log(this.src + " Width: " + this.width);
+
+              if(workingImgLink.search("ww.img")>=0){
+                  workingImgLink = workingImgLink.replace("www.img","i.img");
+                  workingImgLink += '.jpg'
+              }
+              if(workingImgLink.search("/imgur")>=0){
+                  workingImgLink = workingImgLink.replace("/imgur","/i.imgur");
+                  workingImgLink += '.jpg'
+              }
+              if(workingImgLink.search("gifv")>=0){
+                  workingImgLink = workingImgLink.replace("gifv","gif");
+              }
+              if(workingImgLink.search("/gfy")>=0){
+                  workingImgLink = workingImgLink.replace("/gfy","/zippy.gfy");
+                  workingImgLink += '.webm'
+              }
+
+              if (this.width > 0) {
+                console.log(this.width);
+                if (this.src.search("webm") >= 0) {
+                  document.getElementById('photoGrid').innerHTML += '<video autoplay loop class="box-wrapper"> <source  type="video/webm" src= "' + this.src + '" data-url= "' + this.src + '" class="box" style=background-image:url("' + this.src + '")></video>';
+                } else {
+                  document.getElementById('photoGrid').innerHTML += '<div class="box-wrapper"> <div  data-url= "' + this.src + '" class="box" style=background-image:url("' + this.src + '")></div></div>';
+
+                }
+              }
+
+          }
         }
-        $('.tile')
-        // tile mouse actions
-        .on('mouseover', function(){
-          $(this).children('.photo').css({'transform': 'scale('+ $(this).attr('data-scale') +')'});
-        })
-        .on('mouseout', function(){
-          $(this).children('.photo').css({'transform': 'scale(1)'});
-        })
-        .on('mousemove', function(e){
-      $(this).children('.photo').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
-
-     })
-        */
+    } */
